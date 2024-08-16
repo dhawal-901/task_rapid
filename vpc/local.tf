479ed98b15ca2fb094eb8da1e3ed326cd1597967
Arun Lohar <arunlohar@gkmit.co>
2024-08-16 12:05:57 +0530

    asd

--- vpc/local.tf
+++ vpc/local.tf
@@ -1,11 +1,11 @@
 locals {
   config = {
-    dev = {
-      vpc_name        = "vpc_tf_dev"
+    default = {
+      vpc_name        = "task_rapid"
       vpc_cidr        = "10.1.0.0/16"
-      public_subnets  = ["10.1.1.0/24", "10.1.2.0/24"]
-      private_subnets = ["10.1.3.0/24", "10.1.4.0/24"]
-      azs             = ["ap-south-1a", "ap-south-1b"]
+      public_subnets  = ["10.1.1.0/24","10.1.2.0/24"]
+      private_subnets = ["10.1.3.0/24","10.1.4.0/24"]
+      azs             = ["ap-south-1a", "ap-south-1b","ap-south-1c"]
 
       lb_sg_name       = "lb_sq_dev"
       instance_sg_name = "instance_sq_dev"
@@ -15,7 +15,7 @@ locals {
       certificate_arn   = ""
 
       ubuntu_ami           = "ami-0ad21ae1d0696ad58"
-      ubuntu_instance_type = "t2.micro"
+      ubuntu_instance_type = "t2.medium"
 
       #   bucket_name = "my-testing-bucket-${terraform.workspace}-000000"
 
@@ -31,5 +31,5 @@ locals {
     }
   }
 
-  Environment = local.config.dev
+  Environment = local.config.default
 }
