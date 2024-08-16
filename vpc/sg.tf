213948a9b3594f5d7623a06e4e3ffb998256f7bc
Arun Lohar <arunlohar@gkmit.co>
2024-08-16 12:08:01 +0530

    sad

--- vpc/sg.tf
+++ vpc/sg.tf
@@ -3,17 +3,12 @@ resource "aws_security_group" "my_public_sg" {
   name        = local.Environment.lb_sg_name
   description = "Allow 80, 443 only"
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ 
   egress {
     protocol    = "-1"
     from_port   = 0
@@ -30,17 +25,12 @@ resource "aws_security_group" "my_public_sg_2" {
   name        = local.Environment.instance_sg_name
   description = "Allow 22 only"
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    protocol    = "-1"
     from_port   = 0
     to_port     = 0
-    security_groups = [aws_security_group.my_public_sg.id]
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+ 
   egress {
     protocol    = "-1"
     from_port   = 0
