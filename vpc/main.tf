4c472ac8b8d7eb24aa6cc5a14508195af60bf518
Arun Lohar <arunlohar@gkmit.co>
2024-08-16 12:14:11 +0530

    sdf

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,14 +7,9 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket  = "jenkins-task-2-node-app-94144 "
-    encrypt = true
-    key     = "terrbac"
-    region  = "ap-south-1"
-  }
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
+
